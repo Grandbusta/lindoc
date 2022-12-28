24296926abe55a4cecb79bd06a3309e88395142f
Grandbusta <bustajay30@gmail.com>
2022-12-28 01:27:29 +0100

    writing to file

--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fmt.Println("lindoc running")
-	res, err := http.Get("https://blog.logrocket.com/concurrency-patterns-golang-waitgroups-goroutines/")
+	res, err := http.Get("https://betterprogramming.pub/deep-dive-into-concurrency-of-go-93002344d37b")
+	// res, err := http.Get("https://www.keypup.io/blog/3-reasons-why-software-developers-use-social-media")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
 	content, err := ioutil.ReadAll(res.Body)
-	fmt.Println(string(content))
+	con := html.UnescapeString(string(content))
+	fmt.Println(con)
+	f, err := os.Create("index.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	f.Write(content)
 }
